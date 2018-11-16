bb6cfbd76327d8f99df64d610be572f46b614f67
dragkar <34564898+dragkar@users.noreply.github.com>
2018-11-16 15:45:18 +0300

    Create ServiceAdditionOfIntegerNumbers.java

---
+++ task1/ru/tander/operationswithnumbers/services/ServiceAdditionOfIntegerNumbers.java
@@ -0,0 +1,10 @@
+package ru.tander.operationswithnumbers.services;/*
+* Copyright (c) 2018 Tander, All Rights Reserved.
+*/
+
+
+public class ServiceAdditionOfIntegerNumbers {
+	public int additionIntNumbers(int firstNumber, int secondNumber) {
+		return  firstNumber + secondNumber;
+	}
+}
